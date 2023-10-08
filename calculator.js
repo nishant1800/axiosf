621004374c6d844cb582da63bbf6ece9a7598b32
Nishant Ranjan <110740001+nishant1800@users.noreply.github.com>
2023-10-08 22:31:01 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,58 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow", "red", "green", "purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+    if (started == false) {
+        console.log("game is started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash")
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function userFlash(btn) {
+    btn.classsList.add("userFlash");
+    setTimeout(function () {
+        user.classList.remove("user");
+    }, 250);
+}
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    btnFlash(randBtn);
+}
+
+function btnPress() {
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
